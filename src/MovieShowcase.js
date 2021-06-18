5e4f3b41b9d6e13c0ffdcb0c7d0a4ccebbd58ba7
Ahmed Mohammed <ahmed1831990@gmail.com>
2021-06-18 11:44:37 +0000

    Automatically backed up by Learn

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -3,15 +3,6 @@ import MovieCard from './card-components/MovieCard.js';
 import movieData from './data.js';
 
 export default class MovieShowcase extends Component {
-	// generateMovieCards = () => {
-	// 	return movieData.map((movie, index) => <MovieCard
-	// 	      key={index}
-	// 	      title={movie.title}
-	// 	      IMDBRating={movie.IMDBRating}
-	// 	      genres={movie.genres}
-	// 	      poster={movie.poster}
-	// 	    />)
-	// };
 
 	render() {
 		return <div id="movie-showcase">{
